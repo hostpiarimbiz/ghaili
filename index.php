2ad03499ae9ff08c6670e818d306ca5214ef46e3
hostpiarimbiz <hostpiarimbiz@gmail.com>
2017-01-25 22:18:30 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
-$page = explode('/',$_SERVER['REQUEST_URI']);
-if(isset($page[1])){
-	header('Location: '.$page[1]);
-}
+//$page = explode('/',$_SERVER['REQUEST_URI']);
+//if(isset($page[1])){
+	//header('Location: '.$page[1]);
+//}
+
+header('HTTP/1.0 301 Moved Permanently');
+header('Location: '.$_GET['go']);
 ?>
